c1b29e40c016433a88d657ada76961b00d397325
Ergenekon Yigit <ergenekonyigit@hotmail.com>
2017-05-01 04:28:25 +0300

    Add files via upload

--- init.el
+++ init.el
@@ -7,81 +7,78 @@
 
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-(unless (package-installed-p 'use-package)
-	(package-refresh-contents)
-	(package-install 'use-package))
+(unless (package-installed-p 'use-package) 
+  (package-refresh-contents) 
+  (package-install 'use-package))
 
-(use-package try
-        :ensure t
-	)
+(use-package 
+  try 
+  :ensure t)
 
-(use-package which-key
-	:ensure t 
-	:config
-	(which-key-mode)
-	)
+(use-package 
+  elisp-format 
+  :ensure t)
 
-(use-package org-bullets
-        :ensure t
-	:config
-	(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-	)
+(use-package 
+  which-key 
+  :ensure t 
+  :config (which-key-mode))
 
-(use-package ace-window
-        :ensure t
-	:init
-	(progn
-	  (global-set-key [remap other-window] 'ace-window)
-	  (custom-set-faces
-	   '(aw-leading-char-face
-	     ((t (:inherit ace-jump-face-foreground :height 3.0))))) 
-	  ))
+(use-package 
+  org-bullets 
+  :ensure t 
+  :config (add-hook 'org-mode-hook (lambda () 
+				     (org-bullets-mode 1))))
 
-(use-package counsel
-         :ensure t
-	 )
+(use-package 
+  ace-window 
+  :ensure t 
+  :init (progn (global-set-key [remap other-window] 'ace-window) 
+	       (custom-set-faces '(aw-leading-char-face ((t 
+							  (:inherit ace-jump-face-foreground 
+								    :height 3.0)))))))
 
-(use-package ivy
-  :ensure t
-  :diminish (ivy-mode)
-  :bind (("C-x b" . ivy-switch-buffer))
-  :config
-  (ivy-mode 1)
-  (setq ivy-use-virtual-buffers t)
+(use-package 
+  counsel 
+  :ensure t)
+
+(use-package 
+  ivy 
+  :ensure t 
+  :diminish (ivy-mode) 
+  :bind (("C-x b" . ivy-switch-buffer)) 
+  :config (ivy-mode 1) 
+  (setq ivy-use-virtual-buffers t) 
   (setq ivy-display-style 'fancy))
 
-(use-package swiper
-         :ensure try
-	 :bind (("C-s" . swiper)
-		("C-r" . swiper)
-		("C-c C-r" . ivy-resume)
-		("M-x" . counsel-M-x)
-		("C-x C-f" . counsel-find-file))
-	 :config
-	 (progn
-	   (ivy-mode 1)
-	   (setq ivy-use-virtual-buffers t)
-	   (setq ivy-display-style 'fancy)
-	   (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
-	   ))
+(use-package 
+  swiper 
+  :ensure try 
+  :bind (("C-s" . swiper) 
+	 ("C-r" . swiper) 
+	 ("C-c C-r" . ivy-resume) 
+	 ("M-x" . counsel-M-x) 
+	 ("C-x C-f" . counsel-find-file)) 
+  :config (progn (ivy-mode 1) 
+		 (setq ivy-use-virtual-buffers t) 
+		 (setq ivy-display-style 'fancy) 
+		 (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)))
 
-(use-package avy
-         :ensure t
-	 :bind ("M-s" . avy-goto-char))
+(use-package 
+  avy 
+  :ensure t 
+  :bind ("M-s" . avy-goto-char))
 
-(use-package auto-complete
-  :ensure t
-  :init
-  (progn
-    (ac-config-default)
-    (global-auto-complete-mode t)
-    ))
+(use-package 
+  auto-complete 
+  :ensure t 
+  :init (progn (ac-config-default) 
+	       (global-auto-complete-mode t)))
 
-(use-package solarized-theme
-  :ensure t
+(use-package 
+  solarized-theme 
+  :ensure t 
   :config (load-theme 'solarized-dark t))
-
